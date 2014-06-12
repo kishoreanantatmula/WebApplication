5612a9a56bdafcd37d0cba74a2cc010ebb441937
kishoreanantatmula <anantha6445@gmail.com>
2014-06-12 15:51:37 +0530

    Commit  to Github
    Commit to GitHub

---
+++ SpringBoard/src/com/myservlet/servlet1.java
@@ -0,0 +1,64 @@
+package com.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet1
+ */
+public class servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw = response.getWriter();
+		
+		String username = request.getParameter("uname");
+		String password = request.getParameter("pass");
+		
+		
+		if(username.equalsIgnoreCase("kishore")&& password.equalsIgnoreCase("kishore"))
+		{
+			
+		   pw.write("Login Successfull");	
+		   
+		   
+			
+		}
+		
+		else
+		{
+			
+			pw.write("Login Failure");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ SpringBoard/src/com/myservlets/FirstServlaet.java
@@ -0,0 +1,41 @@
+package com.myservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlaet
+ */
+public class FirstServlaet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public FirstServlaet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.write("First Servlet");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
